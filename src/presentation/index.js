faf99d29518fcac2e58e719372fa609839116c2a
Paulo <paulogustavopeixoto@gmail.com>
2025-01-14 14:03:21 +0000

    updated index in presentation folder

--- src/presentation/index.js
+++ src/presentation/index.js
@@ -1,8 +1,12 @@
 const upload = require('./uploadPptx');
 const generateLink = require('./generateLink');
+const duplicate = require('./duplicatePptx');
+const getSlides = require('./slidesExtraction');
 
 
 module.exports = {
     upload,
-    generateLink
+    generateLink,
+    duplicate,
+    getSlides
 }
