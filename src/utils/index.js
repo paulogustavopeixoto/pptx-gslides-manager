dd60a708e8fef0830643e0194d10c443d9da7cbd
Paulo <paulogustavopeixoto@gmail.com>
2025-01-14 16:26:00 +0000

    removed deleted paths

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,7 @@
 const download = require('./fileDownloads');
 const rgbToHex = require('./rgbToHex');
-const processElement = require('./google/processElement');
-const extractParagraphsAndRuns = require('./google/extractParagraphsAndRuns');
 
 module.exports = {
     download,
-    rgbToHex,
-    processElement,
-    extractParagraphsAndRuns
+    rgbToHex
 }
